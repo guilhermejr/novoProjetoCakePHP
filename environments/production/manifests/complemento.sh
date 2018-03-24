a295e03d7f565d7c5e6960d22b5c6f6da4a0623c
Guilherme Jr <falecom@guilhermejr.net>
2018-03-24 08:56:23 -0300

    Atualizada a versão do jQuery

--- environments/production/manifests/complemento.sh
+++ environments/production/manifests/complemento.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # --- Baixa o jquey e o bootstrap ---
-su vagrant -c "wget https://github.com/jquery/jquery/archive/3.2.1.zip -O /vagrant/jquery.zip"
+su vagrant -c "wget https://github.com/jquery/jquery/archive/3.3.1.zip -O /vagrant/jquery.zip"
 su vagrant -c "wget https://github.com/twbs/bootstrap/releases/download/v3.3.7/bootstrap-3.3.7-dist.zip -O /vagrant/bootstrap.zip"
 
 # --- Descompacta arquivos ---
@@ -9,7 +9,7 @@ su vagrant -c "unzip /vagrant/jquery.zip -d /vagrant"
 su vagrant -c "unzip /vagrant/bootstrap.zip -d /vagrant"
 
 # --- Copia para as pastas destino ---
-cp -p /vagrant/jquery-3.2.1/dist/jquery.min.js /vagrant/app/webroot/js/
+cp -p /vagrant/jquery-3.3.1/dist/jquery.min.js /vagrant/app/webroot/js/
 cp -p /vagrant/bootstrap-3.3.7-dist/js/bootstrap.min.js /vagrant/app/webroot/js/
 cp -p /vagrant/bootstrap-3.3.7-dist/js/npm.js /vagrant/app/webroot/js/
 cp -p /vagrant/bootstrap-3.3.7-dist/fonts/* /vagrant/app/webroot/font/
@@ -21,7 +21,7 @@ cp -p /vagrant/bootstrap-3.3.7-dist/css/bootstrap.min.css.map /vagrant/app/webro
 # --- Apaga os arquivos baixados e as pastas em que foram descompactados ---
 rm -rf /vagrant/jquery.zip
 rm -rf /vagrant/bootstrap.zip
-rm -rf /vagrant/jquery-3.2.1
+rm -rf /vagrant/jquery-3.3.1
 rm -rf /vagrant/bootstrap-3.3.7-dist
 
 # --- Mensagem ---
